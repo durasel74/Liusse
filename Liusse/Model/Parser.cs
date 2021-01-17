907897ce0d9492f965d4d46faddf0913aa050d5e
durasel74 <diman174cheb@mail.ru>
2021-01-17 15:11:43 +0500

    Minimal Structure v0.0.1

---
+++ Liusse/Model/Parser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Liusse
+{
+	public class Parser : INotifyPropertyChanged
+	{
+		private string currentExpression = "";
+		private string example = "";
+
+		public string CurrentExpression
+		{
+			get { return currentExpression; }
+			set
+			{
+				currentExpression = value;
+				OnPropertyChanged("CurrentExpression");
+			}
+		}
+		public string Example
+		{
+			get { return example; }
+			set
+			{
+				example = value;
+				OnPropertyChanged("Example");
+			}
+		}
+
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanged([CallerMemberName] string prop = "")
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(prop));
+			}
+		}
+	}
+}
--- Liusse/View/MainWindow.xaml.cs
+++ Liusse/View/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace Liusse
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
